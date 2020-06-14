a646c0278ad50ebbe8170689c1a0b9232c88b099
Chris Fung <aergonaut@gmail.com>
2020-06-13 18:19:32 -0700

    added kitty to home-manager

--- etc/nix/home.nix
+++ etc/nix/home.nix
@@ -3,6 +3,59 @@
 {
   programs.home-manager.enable = true;
 
+  xdg.enable = true;
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      package = pkgs.hack-font;
+      name = "Hack";
+    };
+    settings = {
+      window_padding_width = 5;
+      active_border_color = "#98c379";
+      font_size = 13;
+      macos_thicken_font = "0.5";
+      macos_titlebar_color = "background";
+
+      # colors
+      foreground = "#abb2bf";
+      background = "#1e2127";
+
+      # black
+      color0 = "#1e2127";
+      color8 = "#767676";
+
+      # red
+      color1 = "#e06c75";
+      color9 = "#e06c75";
+
+      # green
+      color2 = "#98c379";
+      color10 = "#98c379";
+
+      # yellow
+      color3 = "#d19a66";
+      color11 = "#d19a66";
+
+      # blue
+      color4 = "#61afef";
+      color12 = "#61afef";
+
+      # magenta
+      color5 = "#c678dd";
+      color13 = "#c678dd";
+
+      # cyan
+      color6 = "#56b6c2";
+      color14 = "#56b6c2";
+
+      # white
+      color7 = "#abb2bf";
+      color15 = "#abb2bf";
+    };
+  };
+
   programs.bat.enable = true;
   programs.fzf.enable = true;
 
