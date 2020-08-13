1908089f1e0052b56a5e298228ac6dba70463375
Chris Fung <aergonaut@gmail.com>
2020-08-13 09:24:24 -0700

    add after-upgrade.sh script to fix environment after macOS upgrades

---
+++ bin/after-upgrade.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo mv /etc/zshenv   /etc/zshenv.bak.before_nix_darwin
+sudo mv /etc/zshrc    /etc/zshrc.bak.before_nix_darwin
+sudo mv /etc/zprofile /etc/zprofile.bak.before_nix_darwin
+
+sudo /nix/var/nix/profiles/system/activate
