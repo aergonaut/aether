417a958edf9627917ff2626783e8ff50f9801252
Chris Fung <aergonaut@gmail.com>
2020-12-16 16:37:42 -0800

    move extensions to separate file

---
+++ etc/nix/home/extensions.nix
@@ -0,0 +1,74 @@
+[
+  {
+    name = "vim";
+    publisher = "vscodevim";
+    version = "1.18.4";
+    sha256 = "1j5wp9mq9n4mykh2a1frx0y4z9mq748mjadrlbyx7w5pgk8ss2cv";
+  }
+  {
+    name = "nix";
+    publisher = "bbenoist";
+    version = "1.0.1";
+    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  }
+  {
+    name = "vscode-eslint";
+    publisher = "dbaeumer";
+    version = "2.1.13";
+    sha256 = "1adzgisljlq6xmmwrziawkgy941bf8i8nq4c9w3gnsbr20gd9pyi";
+  }
+  {
+    name = "gitlens";
+    publisher = "eamodio";
+    version = "10.2.1";
+    sha256 = "1bh6ws20yi757b4im5aa6zcjmsgdqxvr1rg86kfa638cd5ad1f97";
+  }
+  {
+    name = "file-icons";
+    publisher = "file-icons";
+    version = "1.0.24";
+    sha256 = "0mcaz4lv7zb0gw0i9zbd0cmxc41dnw344ggwj1wy9y40d627wdcx";
+  }
+  {
+    name = "atom-keybindings";
+    publisher = "ms-vscode";
+    version = "3.0.6";
+    sha256 = "0r5brd9iirfs3zaqq1swlx2n3mqxzyggv4rzjdyjh8zsyi1lfjas";
+  }
+  {
+    name = "github-vscode-theme";
+    publisher = "github";
+    version = "1.1.5";
+    sha256 = "1llyrm4iwdp79ylnw4kn5g6qlp8blrpaddszhl61yv82rs60kw0h";
+  }
+  {
+    name = "prettier-vscode";
+    publisher = "esbenp";
+    version = "5.7.2";
+    sha256 = "1wf4dwh11pz2hcy26kvhqnp2bhnk6n0pjh2qs0jsn8sl16656f1f";
+  }
+  {
+    name = "haml";
+    publisher = "karunamurti";
+    version = "1.3.1";
+    sha256 = "1d5143m9ysn0s8q2c06gw7fswk5mah1p3g2gcrg93117y7ky24s3";
+  }
+  {
+    name = "sorbet-lsp";
+    publisher = "danhuynhdev";
+    version = "0.1.5";
+    sha256 = "110q52vgan5alf0gzxwg1a4z8qxwr5ry23gf5ma37j5mxdhvw194";
+  }
+  {
+    name = "vscode-tailwindcss";
+    publisher = "bradlc";
+    version = "0.5.6";
+    sha256 = "0hvga74mddvncq2ngzx9zz1f7al45vjx63968sw655r50za9jaml";
+  }
+  {
+    name = "vscode-open-in-github";
+    publisher = "fabiospampinato";
+    version = "1.2.3";
+    sha256 = "0xij1qgx89namw2w2kqw7c9rvspckiz16fmpdyvcmmx36cxqwkgd";
+  }
+]
