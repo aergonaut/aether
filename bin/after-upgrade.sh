f22aed861e97d9a0b04de8cf6dd52c6e1b859aac
Chris Fung <aergonaut@gmail.com>
2021-01-13 18:42:04 -0800

    add /etc/shells to list of paths to fix

--- bin/after-upgrade.sh
+++ bin/after-upgrade.sh
@@ -3,5 +3,6 @@
 sudo mv /etc/zshenv   /etc/zshenv.bak.before_nix_darwin
 sudo mv /etc/zshrc    /etc/zshrc.bak.before_nix_darwin
 sudo mv /etc/zprofile /etc/zprofile.bak.before_nix_darwin
+sudo mv /etc/shells   /etc/shells.bak.before_nix_darwin
 
 sudo /nix/var/nix/profiles/system/activate
