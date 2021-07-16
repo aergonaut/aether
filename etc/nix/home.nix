68ad3c8cc7895ce043b2209319b43fdd44c59acd
Chris Fung <aergonaut@gmail.com>
2021-07-16 16:30:52 -0700

    update nix config for neovim 0.5

--- etc/nix/home.nix
+++ etc/nix/home.nix
@@ -403,9 +403,9 @@ in
 
   programs.neovim = {
     enable = true;
-    package = pkgs.neovim-nightly;
     vimAlias = true;
     withRuby = false;
+    withPython3 = false;
     extraConfig = ''
       set termguicolors
 
@@ -449,7 +449,7 @@ in
       nmap <silent> <Esc> <Esc>:nohlsearch<CR>
 
       " copy filepath variants
-      nmap <silent> <Leader>C :let @*=expand("%") \| echohl String \| echon 'Copied: ' . expand("%") \| echohl None<CR>
+      nmap <silent> <space>C :let @*=expand("%") \| echohl String \| echon 'Copied: ' . expand("%") \| echohl None<CR>
 
       function! s:ThankYouNext() abort
         update
@@ -510,7 +510,7 @@ in
       {
         plugin = vim-fugitive;
         config = ''
-          nnoremap <silent><Leader>gs :Git<CR>
+          nnoremap <silent><space>gs :Git<CR>
         '';
       }
 
@@ -576,10 +576,10 @@ in
             }
           EOF
 
-          nnoremap <leader>ff <cmd>Telescope find_files<cr>
-          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-          nnoremap <leader>fb <cmd>Telescope buffers<cr>
-          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+          nnoremap <space>ff <cmd>Telescope find_files<cr>
+          nnoremap <space>fg <cmd>Telescope live_grep<cr>
+          nnoremap <space>fb <cmd>Telescope buffers<cr>
+          nnoremap <space>fh <cmd>Telescope help_tags<cr>
         '';
       }
 
@@ -603,210 +603,8 @@ in
           EOF
         '';
       }
-      # {
-      #   plugin = vim-airline-head;
-      #   config = ''
-      #     let g:airline_experimental = 0
-      #     let g:airline_powerline_fonts = 1
-      #   '';
-      # }
-      # vim-airline-themes
+
       nvim-web-devicons
-      #       {
-      #         plugin = galaxyline-nvim;
-      #         config = ''
-      #           lua << EOF
-      #             local gl = require('galaxyline')
-      #             local gls = gl.section
-      #             gl.short_line_list = {'LuaTree','vista','dbui'}
-      # 
-      #             local colors = {
-      #               bg = '#282c34',
-      #               yellow = '#fabd2f',
-      #               cyan = '#008080',
-      #               darkblue = '#081633',
-      #               green = '#afd700',
-      #               orange = '#FF8800',
-      #               purple = '#5d4d7a',
-      #               magenta = '#d16d9e',
-      #               grey = '#c0c0c0',
-      #               blue = '#0087d7',
-      #               red = '#ec5f67'
-      #             }
-      # 
-      #             local buffer_not_empty = function()
-      #               if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-      #                 return true
-      #               end
-      #               return false
-      #             end
-      # 
-      #             gls.left[1] = {
-      #               FirstElement = {
-      #                 provider = function() return '▋' end,
-      #                 highlight = {colors.blue,colors.yellow}
-      #               },
-      #             }
-      #             gls.left[2] = {
-      #               ViMode = {
-      #                 provider = function()
-      #                   local alias = {n = 'NORMAL',i = 'INSERT',c= 'COMMAND',v= 'VISUAL',V= 'VISUAL LINE', [''] = 'VISUAL BLOCK'}
-      #                   return alias[vim.fn.mode()]
-      #                 end,
-      #                 separator = '',
-      #                 separator_highlight = {colors.purple,function()
-      #                   if not buffer_not_empty() then
-      #                     return colors.purple
-      #                   end
-      #                   return colors.darkblue
-      #                 end},
-      #                 highlight = {colors.darkblue,colors.purple,'bold'},
-      #               },
-      #             }
-      #             gls.left[3] ={
-      #               FileIcon = {
-      #                 provider = 'FileIcon',
-      #                 condition = buffer_not_empty,
-      #                 highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.darkblue},
-      #               },
-      #             }
-      #             gls.left[4] = {
-      #               FileName = {
-      #                 provider = {'FileName','FileSize'},
-      #                 condition = buffer_not_empty,
-      #                 separator = '',
-      #                 separator_highlight = {colors.purple,colors.darkblue},
-      #                 highlight = {colors.magenta,colors.darkblue}
-      #               }
-      #             }
-      # 
-      #             gls.left[5] = {
-      #               GitIcon = {
-      #                 provider = function() return '  ' end,
-      #                 condition = buffer_not_empty,
-      #                 highlight = {colors.orange,colors.purple},
-      #               }
-      #             }
-      #             gls.left[6] = {
-      #               GitBranch = {
-      #                 provider = 'GitBranch',
-      #                 condition = buffer_not_empty,
-      #                 highlight = {colors.grey,colors.purple},
-      #               }
-      #             }
-      # 
-      #             local checkwidth = function()
-      #               local squeeze_width  = vim.fn.winwidth(0) / 2
-      #               if squeeze_width > 40 then
-      #                 return true
-      #               end
-      #               return false
-      #             end
-      # 
-      #             gls.left[7] = {
-      #               DiffAdd = {
-      #                 provider = 'DiffAdd',
-      #                 condition = checkwidth,
-      #                 icon = ' ',
-      #                 highlight = {colors.green,colors.purple},
-      #               }
-      #             }
-      #             gls.left[8] = {
-      #               DiffModified = {
-      #                 provider = 'DiffModified',
-      #                 condition = checkwidth,
-      #                 icon = ' ',
-      #                 highlight = {colors.orange,colors.purple},
-      #               }
-      #             }
-      #             gls.left[9] = {
-      #               DiffRemove = {
-      #                 provider = 'DiffRemove',
-      #                 condition = checkwidth,
-      #                 icon = ' ',
-      #                 highlight = {colors.red,colors.purple},
-      #               }
-      #             }
-      #             gls.left[10] = {
-      #               LeftEnd = {
-      #                 provider = function() return '' end,
-      #                 separator = '',
-      #                 separator_highlight = {colors.purple,colors.bg},
-      #                 highlight = {colors.purple,colors.purple}
-      #               }
-      #             }
-      #             gls.left[11] = {
-      #               DiagnosticError = {
-      #                 provider = 'DiagnosticError',
-      #                 icon = '  ',
-      #                 highlight = {colors.red,colors.bg}
-      #               }
-      #             }
-      #             gls.left[12] = {
-      #               Space = {
-      #                 provider = function () return ' ' end
-      #               }
-      #             }
-      #             gls.left[13] = {
-      #               DiagnosticWarn = {
-      #                 provider = 'DiagnosticWarn',
-      #                 icon = '  ',
-      #                 highlight = {colors.blue,colors.bg},
-      #               }
-      #             }
-      #             gls.right[1]= {
-      #               FileFormat = {
-      #                 provider = 'FileFormat',
-      #                 separator = '',
-      #                 separator_highlight = {colors.bg,colors.purple},
-      #                 highlight = {colors.grey,colors.purple},
-      #               }
-      #             }
-      #             gls.right[2] = {
-      #               LineInfo = {
-      #                 provider = 'LineColumn',
-      #                 separator = ' | ',
-      #                 separator_highlight = {colors.darkblue,colors.purple},
-      #                 highlight = {colors.grey,colors.purple},
-      #               },
-      #             }
-      #             gls.right[3] = {
-      #               PerCent = {
-      #                 provider = 'LinePercent',
-      #                 separator = '',
-      #                 separator_highlight = {colors.darkblue,colors.purple},
-      #                 highlight = {colors.grey,colors.darkblue},
-      #               }
-      #             }
-      #             gls.right[4] = {
-      #               ScrollBar = {
-      #                 provider = 'ScrollBar',
-      #                 highlight = {colors.yellow,colors.purple},
-      #               }
-      #             }
-      # 
-      #             gls.short_line_left[1] = {
-      #               BufferType = {
-      #                 provider = 'FileTypeName',
-      #                 separator = '',
-      #                 separator_highlight = {colors.purple,colors.bg},
-      #                 highlight = {colors.grey,colors.purple}
-      #               }
-      #             }
-      # 
-      # 
-      #             gls.short_line_right[1] = {
-      #               BufferIcon = {
-      #                 provider= 'BufferIcon',
-      #                 separator = '',
-      #                 separator_highlight = {colors.purple,colors.bg},
-      #                 highlight = {colors.grey,colors.purple}
-      #               }
-      #             }
-      # 
-      #           EOF
-      #         '';
-      #       }
 
       vim-conflicted
       vim-system-copy
@@ -953,10 +751,10 @@ in
 
               -- Set some keybinds conditional on server capabilities
               if client.resolved_capabilities.document_formatting then
-                buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+                buf_set_keymap("n", "<space>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
               end
               if client.resolved_capabilities.document_range_formatting then
-                buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+                buf_set_keymap("v", "<space>lf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
               end
 
               -- Set autocommands conditional on server_capabilities
