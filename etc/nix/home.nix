47470139148a8810b08d9d283b1842e5882e34c7
Chris Fung <aergonaut@gmail.com>
2022-01-06 10:40:27 -0800

    add custom rubocop source with fixed column info

--- etc/nix/home.nix
+++ etc/nix/home.nix
@@ -807,13 +807,72 @@ in
             -- Attach null-ls
             local null_ls = require("null-ls")
 
+            local h = require("null-ls.helpers")
+            local methods = require("null-ls.methods")
+
+            -- Custom RuboCop source with start/end column fix
+            local handle_rubocop_output = function(params)
+              if params.output and params.output.files then
+                local file = params.output.files[1]
+                if file and file.offenses then
+                  local parser = h.diagnostics.from_json({
+                    severities = {
+                      info = h.diagnostics.severities.information,
+                      refactor = h.diagnostics.severities.hint,
+                      convention = h.diagnostics.severities.warning,
+                      warning = h.diagnostics.severities.warning,
+                      error = h.diagnostics.severities.error,
+                      fatal = h.diagnostics.severities.fatal,
+                    },
+                  })
+                  local offenses = {}
+
+                  for _, offense in ipairs(file.offenses) do
+                    table.insert(offenses, {
+                      message = offense.message,
+                      ruleId = offense.cop_name,
+                      level = offense.severity,
+                      line = offense.location.start_line,
+                      column = offense.location.start_column,
+                      endLine = offense.location.last_line,
+                      endColumn = offense.location.last_column + 1,
+                    })
+                  end
+
+                  return parser({ output = offenses })
+                end
+              end
+
+              return {}
+            end
+
+            local my_rubocop = {
+              name = "RuboCop",
+              method = null_ls.methods.DIAGNOSTICS,
+              filetypes = { "ruby" },
+              generator = null_ls.generator({
+                command = "bundle",
+                args = { "exec", "rubocop", "-f", "json", "--stdin", "$FILENAME" },
+                timeout = 10000,
+                to_stdin = true,
+                format = "json",
+                check_exit_code = function(code)
+                  return code <= 1
+                end,
+                on_output = handle_rubocop_output
+              })
+            }
+
             local sources = {
               -- diagnostics
+              my_rubocop,
+              --[[
               null_ls.builtins.diagnostics.rubocop.with({
                 command = "bundle",
                 args = { "exec", "rubocop" , "-f", "json", "--stdin", "$FILENAME" },
                 timeout = 10000
               }),
+              --]]
 
               -- formatting
               -- code actions
